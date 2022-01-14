64d2015ae21e6e2d9636c8918d2848e009f1f5aa
Thiago Matos <thiagomatos@pop-os.localdomain>
2022-01-13 23:50:07 -0300

    implements basic math operatios

--- 002-calculadora/src/main/java/com/github/thiagohmatos/foo/MathController.java
+++ 002-calculadora/src/main/java/com/github/thiagohmatos/foo/MathController.java
@@ -17,8 +17,53 @@ public class MathController {
         return sum;
     }
 
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double substraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Invalid number informed");
+        }
+        final Double substraction = convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return substraction;
+    }
+
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Invalid number informed");
+        }
+        final Double multiplication = convertToDouble(numberOne) * convertToDouble(numberTwo);
+        return multiplication;
+    }
+
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if ((!isNumeric(numberOne) || !isNumeric(numberTwo)) || (isNumeric(numberTwo) && numberTwo.equals("0"))) {
+            throw new UnsupportedOperationException("Invalid number informed");
+        }
+        final Double division = convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return division;
+    }
+
+    @RequestMapping(value = "/average/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double average(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Invalid number informed");
+        }
+        final Double average = (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+        return average;
+    }
+
+    @RequestMapping(value = "/square/{number}", method = RequestMethod.GET)
+    public Double square(@PathVariable("numberOne") String number) throws Exception {
+        if (!isNumeric(number)) {
+            throw new UnsupportedOperationException("Invalid number informed");
+        }
+        final Double square = convertToDouble(number) * convertToDouble(number);
+        return square;
+    }
+
     private Double convertToDouble(String numberToConvert) {
-        if(numberToConvert == null ) return 0D;
+        if (numberToConvert == null) return 0D;
         String number = numberToConvert.replaceAll(",", ".");
         if (isNumeric(numberToConvert)) {
             return Double.parseDouble(numberToConvert);
@@ -28,7 +73,7 @@ public class MathController {
     }
 
     private boolean isNumeric(String numberToValidate) {
-        if(numberToValidate == null) return false;
+        if (numberToValidate == null) return false;
         String number = numberToValidate.replaceAll(",", ".");
         return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
